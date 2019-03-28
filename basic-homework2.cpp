bc5f0691c7c5896e9efde235763e842331832dc2
yuwen4514 <48576822+yuwen4514@users.noreply.github.com>
2019-03-29 01:09:06 +0800

    Add files via upload

---
+++ basic-homework2.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+
+int main() 
+{
+ int year,month,day; 
+ printf("請輸入你的出生年分(西元)：");
+ scanf("%d",&year);  
+ 
+ printf("請輸入你的出生月分：");
+ scanf("%d",&month); 
+ 
+ printf("請輸入你的出生日期：");
+ scanf("%d",&day);  
+ 
+ printf("你的出生年月日為：%d年%d月%d日\n",year,month,day); 
+ system("pause");    
+ return 0; 
+}
